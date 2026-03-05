97e3b493dc59da689f78c24cd1644fb65b356009
Angel <angeltoro736@gmail.com>
2026-03-04 20:30:48 -0500

    feat: add TypeORM integration to Facultad module

--- edu-space/src/modules/facultad/facultad.module.ts
+++ edu-space/src/modules/facultad/facultad.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Facultad } from './facultad.entity';
 import { FacultadController } from './facultad.controller';
 import { FacultadService } from './facultad.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Facultad])],
   controllers: [FacultadController],
   providers: [FacultadService]
 })
