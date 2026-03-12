21d23edc0bf3d471d22feaa82293a65ba8d446fa
Angel <angeltoro736@gmail.com>
2026-03-11 20:37:18 -0500

    feat: agregar módulo de base de datos y configurar conexión a MySQL

--- edu-space/src/app.module.ts
+++ edu-space/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
 import { FacultadModule } from './modules/facultad/facultad.module';
 import { DocenteModule } from './modules/docente/docente.module';
 import { EspacioModule } from './modules/espacio/espacio.module';
@@ -11,16 +11,7 @@ import { ReservaModule } from './modules/reserva/reserva.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT || '3306', 10),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
+    DatabaseModule,
     FacultadModule,
     DocenteModule,
     EspacioModule,
